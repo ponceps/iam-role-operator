c2c6d9a151d653c55200f4bde978747be40b6346
Lucas Ponce <ponce.ps@gmail.com>
2020-12-08 17:27:19 -0300

    feat: attach managed policies

--- controllers/iamrole_controller.go
+++ controllers/iamrole_controller.go
@@ -99,6 +99,12 @@ func (r *IamRoleReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, err
 	}
 
+	// Update managed policies
+	if err := r.updateManagedPolicies(iamRole); err != nil {
+		log.Error(err, "Failed to update managed policies")
+		return ctrl.Result{}, err
+	}
+
 	// Create or update service account
 	if err := r.CreateOrUpdateServiceAccount(ctx, iamRole); err != nil {
 		log.Error(err, "Error to update service account", "ServiceAccount", iamRole.Spec.ServiceAccount)
